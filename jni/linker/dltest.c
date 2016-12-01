f0386a8e717fc277cd86aaedb2a68477ee3ca6fa
WANG Zhenhua <dev@jackwish.net>
2016-12-01 15:24:10 +0800

    linker: update a log

--- jni/linker/dltest.c
+++ jni/linker/dltest.c
@@ -11,12 +11,12 @@ jstring Java_com_young_apkdemo_LinkerActivity_dlUnwindFindExidx(JNIEnv *env, job
     char* ret = NULL;
     int pcount;
     void* libdl = dlopen("libdl.so", RTLD_NOW);
-    void* sym = dlsym(libdl, "dl_unwind_find_exidx");
+    void* (*sym)(void* pc, int* count) = dlsym(libdl, "dl_unwind_find_exidx");
     if (sym == NULL) {
-        ret = "[fail] to get func dl_unwind_find_exidx from libdl.so";
+        ret = "[fail] to get func dl_unwind_find_exidx from libdl.so, a non-ARM devices?";
         goto done;
     }
-    void* exidx = (void*)dl_unwind_find_exidx(sym, &pcount);
+    void* exidx = sym(sym, &pcount);
     if (exidx == NULL) {
         ret = "[fail] to find the .ARM.exidx section of libdl.so";
     } else {
